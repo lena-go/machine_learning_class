71b2ed3c5392532c5c2fae6be9ee310d01806a6d
xenoturbellida <ElPGordeeva@stud.kpfu.ru>
2023-12-10 13:15:06 +0300

    vertical divider case

--- hws/svm/drawing.py
+++ hws/svm/drawing.py
@@ -8,15 +8,14 @@ class Drawer:
     def __init__(self, clf):
         self.cur_fig = 0
         self.clf = clf
-        w = self.clf.coef_[0]  # vector orthogonal to the hyperplane
-        self.a = -w[0] / w[1]  # y = ax + b
-        self.b = - (clf.intercept_[0]) / w[1]
+        self.w = np.hstack([self.clf.coef_[0], clf.intercept_[0]])  # vector orthogonal to the hyperplane
 
     def draw_sup(self, xx) -> None:
         c = self.clf.support_vectors_[0]
-        yy_down = self.a * xx + (c[1] - self.a * c[0])
+        a = - self.w[0] / self.w[1]
+        yy_down = a * xx + (c[1] - a * c[0])
         c = self.clf.support_vectors_[-1]
-        yy_up = self.a * xx + (c[1] - self.a * c[0])
+        yy_up = a * xx + (c[1] - a * c[0])
 
         plt.plot(xx, yy_down, 'k--', alpha=0.6)
         plt.plot(xx, yy_up, 'k--', alpha=0.6)
@@ -30,19 +29,27 @@ class Drawer:
             edgecolors="k"
         )
 
-    def draw(self, X, y, display_sup: bool = False) -> None:
+    def draw(self, X, y) -> None:
         plt.figure(self.cur_fig)
         self.cur_fig += 1
-        min_x = np.min(X[:, 0])
-        max_x = np.max(X[:, 0])
-        xx = np.linspace(math.floor(min_x), math.ceil(max_x))
 
-        yy = self.a * xx + self.b
+        if self.w[1] == 0:
+            num_points = 50
+            x = - self.w[2] / self.w[0]
+            xx = np.array([x] * num_points)
+            min_y = np.min(X[:, 1])
+            max_y = np.max(X[:, 1])
+            yy = np.linspace(math.floor(min_y), math.ceil(max_y), num_points)
+        else:
+            min_x = np.min(X[:, 0])
+            max_x = np.max(X[:, 0])
+            xx = np.linspace(math.floor(min_x), math.ceil(max_x))
+            a = - self.w[0] / self.w[1]
+            b = - self.w[2] / self.w[1]
+            yy = a * xx + b
+
         plt.plot(xx, yy, 'k-', alpha=0.6)
         plt.scatter(X[:, 0], X[:, 1], c=y, s=30, cmap=plt.cm.Set3)
-
-        if display_sup:
-            self.draw_sup(xx)
 
         plt.axis('tight')
 
--- hws/svm/main.py
+++ hws/svm/main.py
@@ -13,12 +13,17 @@ def randomize_points(n: int, mode: int = 0):
         return make_blobs(n_samples=n, centers=2)  # random_state=6
     if mode == 1:
         # np.random.seed(0)
-        X = np.r_[
-            np.random.randn(half, 2) - [2, 2],
-            np.random.randn(half, 2) + [2, 2]
-        ]
+        # X = np.r_[
+        #     np.random.randn(half, 2) - [2, 2],
+        #     np.random.randn(half, 2) + [2, 2]
+        # ]
+        # X = np.array([[0.0, 0.5], [0.0, 0.0], [5.0, 5.5], [5.0, 0.0]])
+        X = np.array([[-1, -1], [-1, 1], [1, 1], [1, -1]])
+        # X = np.array([[0, -1], [0, 1], [2, 1], [2, -1]])
         print(X)
-        y = [0]*half + [1]*half
+        # y = [0]*half + [1]*half
+        y = np.array([0, 0, 1, 1])  # vertical line
+        # y = np.array([0, 1, 1, 0])  # horizontal line
         return X, y
 
 
@@ -34,13 +39,13 @@ def add_new_point(clf, X, y, point: [int, int] = None):
 
 
 def run():
-    X, y = randomize_points(50, mode=0)
+    X, y = randomize_points(50, mode=1)
     clf = svm.SVC(kernel="linear", C=1000)
     clf.fit(X, y)
     my_plot = Drawer(clf)
-    my_plot.draw(X, y, True)
+    my_plot.draw(X, y)
     new_X, new_y = add_new_point(clf, X, y)
-    my_plot.draw(new_X, new_y, True)
+    my_plot.draw(new_X, new_y)
     my_plot.show()
 
 
